cdf77fe59cafa55f65af8f11e021961bc7f43eb5
Mark Golbeck <golbeckm@cse.msu.edu>
2018-04-15 23:54:52 -0400

    Adding files to project

---
+++ Python/the self-taught programmer/chapter_21_challenge_1.py
@@ -0,0 +1,34 @@
+
+
+class Stack:
+
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        last_item = len(self.items)-1
+        return self.items[last_item]
+
+    def size(self):
+        return len(self.items)
+
+stack = Stack()
+
+for letter in "yesterday":
+    stack.push(letter)
+
+reverse = ""
+
+for index in range(len(stack.items)):
+    reverse += stack.pop()
+
+print(reverse)
